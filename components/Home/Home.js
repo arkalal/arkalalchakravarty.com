a580bb3d6ea14acc52329db519db3d4f263736ff
arkalal <arkalal.123@gmail.com>
2024-09-23 15:51:40 +0530

    testing

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Buiding arkalalchakravarty.com</h2>
+      <h2>Buiding arkalalchakravarty.com 🚀🧑🏻‍💻</h2>
     </div>
   );
 };

b7c44da3459a8981bab3bb40769f292a3bf0f235
arkalal <arkalal.123@gmail.com>
2024-09-26 16:40:46 +0530

    updated custom font

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Buiding arkalalchakravarty.com 🚀🧑🏻‍💻</h2>
+      <h2>Building arkalalchakravarty.com 🚀🧑🏻‍💻</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis natus
+        doloremque commodi? Eos sint et ratione itaque, deserunt earum optio.
+      </p>
     </div>
   );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,51 @@
 import localFont from "next/font/local";
 import "./globals.scss";
-import { GeistSans } from "geist/font";
 
 export const metadata = {
   title: "Arka Lal Chakravarty",
   description: "My services and portfolio",
 };
 
+const CustomFont = localFont({
+  src: [
+    {
+      path: "./fonts/07a54048a9278940-s.p.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/07a54048a9278940-s.woff2",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "./fonts/4f2204fa15b9b11a-s.p.woff2",
+      weight: "600",
+      style: "semi-bold",
+    },
+    {
+      path: "./fonts/90475aac776488b6-s.p.woff2",
+      weight: "600",
+      style: "semi-itlaic",
+    },
+    {
+      path: "./fonts/a34f9d1faa5f3315-s.p.woff2",
+      weight: "600",
+      style: "custom",
+    },
+    {
+      path: "./fonts/CircularXXWeb-Book.woff2",
+      weight: "500",
+      style: "circular",
+    },
+  ],
+  variable: "--font-custom", // Optional: you can define a CSS variable for custom font
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body className={CustomFont.className}>{children}</body>
     </html>
   );
 }
